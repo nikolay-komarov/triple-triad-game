537ea9948f96fbed1bff521882d046575798d880
Nikolay Komarov <kny.komarov@gmail.com>
2022-04-14 22:27:42 +0300

    добавит компонент Heading

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import cn from 'classnames';
+import s from './Heading.module.css';
+
+export const Heading = ({
+    level,
+    className,
+    children
+}) => {
+
+    return React.createElement(
+        `h${level}`,
+        {
+            className: cn(s.root, className, s[`h${level}`])
+        },
+        children
+    );
+};
+
+Heading.propTypes = {
+    level: PropTypes.oneOf([1, 2, 3, 4, 5]).isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Container from "../Container";
+import Heading from "../Heading";
 import s from './Slider.module.css';
+import logo from '../../assets/logo.png';
 
 export const Slider = () => {
     return (
         <section className={s.section}>
             <div className={s.slider}>
                 <Container className={s.sliderContent}>
-                    <h1 className={s.header}>Wow</h1>
-                    <h2 className={s.subheader}>Wow.Wow.Wow</h2>
+                    <Heading level={2} className={s.header}>WOW</Heading>
+                    <img
+                        src={logo}
+                        alt='logo'
+                        width='640'
+                        height='360'
+                    />
                     <div className={s.call}>
-                        <button className={s.button}>Wow</button>
+                        <button className={s.button}>WOW</button>
                     </div>
                 </Container>
             </div>
