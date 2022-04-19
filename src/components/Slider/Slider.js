a0e1fdb1c154146ad2f5a4417fe199726a706435
Nikolay Komarov <kny.komarov@gmail.com>
2022-04-19 12:32:41 +0300

    добавит state в Slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '../Container';
 import Heading from '../Heading';
 import logo from '../../assets/logo.png';
 import s from './Slider.module.scss';
 
 export const Slider = () => {
+    const [value, setValue] = useState(0);
+
+    const handleClick = () => {
+        setValue(prev => ++prev);
+    };
+
     return (
         <section className={s.section}>
             <div className={s.slider}>
@@ -16,8 +22,9 @@ export const Slider = () => {
                         width="640"
                         height="360"
                     />
+                    <Heading level={3} className={s.subheader}>{value}</Heading>
                     <div className={s.call}>
-                        <button className={s.button}>WOW</button>
+                        <button className={s.button} onClick={handleClick}>WOW</button>
                     </div>
                 </Container>
             </div>
