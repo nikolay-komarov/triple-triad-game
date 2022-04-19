566778b1553a5d34d67dde4313cc8cf0b93ce74d
Nikolay Komarov <kny.komarov@gmail.com>
2022-04-19 12:01:51 +0300

    добавит пропсы в карточку героя

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import Heading from './components/Heading';
 import CharacterCard from './components/CharacterCard';
 import s from './App.module.scss';
 
+const CHARACTER = {
+    id: 1011334,
+    name: 'Spider-Man',
+    description: 'Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.',
+    thumbnail: {
+        path: 'https://firebasestorage.googleapis.com/v0/b/it-course-84ddd.appspot.com/o/marvel-game%2Fspider-man.png?alt=media&token=8ff4b083-81ed-449f-823c-c79110735d1b'
+    },
+    humanName: 'Peter Parker',
+    isLike: false,
+};
+
 const App = () => {
     return (
         <>
@@ -23,9 +34,9 @@ const App = () => {
                         </Heading>
                     </div>
                     <div className={s.cardWrapper}>
-                        <div><CharacterCard /></div>
-                        <div><CharacterCard /></div>
-                        <div><CharacterCard /></div>
+                        <div><CharacterCard src={CHARACTER.thumbnail.path} {...CHARACTER}/></div>
+                        <div><CharacterCard src={CHARACTER.thumbnail.path} {...CHARACTER}/></div>
+                        <div><CharacterCard src={CHARACTER.thumbnail.path} {...CHARACTER}/></div>
                     </div>
                 </Container>
             </section>
--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,43 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {ReactComponent as HeartLogo} from './assets/heart.svg';
 import Heading from '../Heading';
 import Text from '../Text';
 import s from './CharacterCard.module.scss';
 
-const CHARACTER = {
-    id: 1011334,
-    name: 'Spider-Man',
-    description: 'Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.',
-    thumbnail: {
-        path: 'https://firebasestorage.googleapis.com/v0/b/it-course-84ddd.appspot.com/o/marvel-game%2Fspider-man.png?alt=media&token=8ff4b083-81ed-449f-823c-c79110735d1b'
-    },
-    humanName: 'Peter Parker',
-    isLike: false,
-};
+// todo: add s.readBio
 
-export const CharacterCard = () => {
+export const CharacterCard = ({
+    id, // todo: add
+    name,
+    src,
+    description,
+    humanName,
+}) => {
     return (
         <div className={s.root}>
             <img
                 className={s.cardImage}
-                src={CHARACTER.thumbnail.path}
-                alt={CHARACTER.name}
+                src={src}
+                alt={name}
             />
             <div className={s.cardDetails}>
                 <Heading
                     className={s.cardName}
                     level={2}
                 >
-                    {CHARACTER.name}
+                    {name}
                 </Heading>
                 <Heading
                     className={s.cardHumanName}
                     level={3}
                 >
-                    {CHARACTER.humanName}
+                    {humanName}
                 </Heading>
                 <Text className={s.cardDescription}>
-                    {CHARACTER.description}
+                    {description}
                 </Text>
                 <div className={s.cardMeta}>
                     <div className={s.like}>
@@ -50,4 +48,12 @@ export const CharacterCard = () => {
             </div>
         </div>
     );
+};
+
+CharacterCard.propTypes = {
+    id: PropTypes.string,
+    name: PropTypes.string,
+    src: PropTypes.string,
+    description: PropTypes.string,
+    humanName: PropTypes.string,
 };
