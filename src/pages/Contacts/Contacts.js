cd87f5d1d63deb8806e58732feda282b8fe700de
Nikolay Komarov <kny.komarov@gmail.com>
2022-05-03 23:05:19 +0300

    добавит контент на странице contacts

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Container from '../../components/Container';
+import Heading from '../../components/Heading';
+import Brick from '../../components/Brick';
+import picImg from '../../assets/contacts.png';
 import s from './Contacts.module.scss';
 
 export const Contacts = () => {
     return (
         <div className={s.root}>
-            ContactsPage
+            <Container>
+                <Brick />
+                <Heading>Комаров Николай</Heading>
+                <img src={picImg} alt="contacs pic" width="176" height="176"/>
+                <Brick />
+                <Heading level={3}>contacts:</Heading>
+                <Brick />
+                <a href="https://github.com/nikolay-komarov">github.com/nikolay-komarov</a>
+                <Brick />
+                <a href="https://career.habr.com/kny-komarov" >career.habr.com/kny-komarov</a>
+                <Brick />
+            </Container>
         </div>
     );
 };
