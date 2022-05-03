0bdefecfa35f146e53d770e22b36d325f791eb37
Nikolay Komarov <kny.komarov@gmail.com>
2022-05-03 17:54:38 +0300

    добавит роутинг в app и перенесет логику из app в main

--- src/App.js
+++ src/App.js
@@ -1,78 +1,15 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import Slider from './components/Slider';
-import Footer from './components/Footer';
-import Container from './components/Container';
-import Heading from './components/Heading';
-import CharacterCard from './components/CharacterCard';
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 import Biography from './pages/Biography';
-import {CHARACTERS} from './mocks/characters';
-import s from './App.module.scss';
+import Main from './pages/Main';
 
 const App = () => {
-    const [showedIdCharacter, setShowedIdCharacter] = useState(null);
-    const [characters, setCharacters] = useState(CHARACTERS);
-    const handleLikeClick = (id) => {
-        setCharacters(prevState => prevState.map(item => {
-                if (item.id === id) {
-                    return {
-                        ...item,
-                        isLike: !item.isLike
-                    }
-                }
-                return item;
-            })
-        );
-    };
-
-    const handleSetShowCharacterBio = (id) => setShowedIdCharacter(id);
-
     return (
-        <>
-            <Header />
-            {
-                !!showedIdCharacter
-                ? (
-                    <Biography
-                        id={showedIdCharacter}
-                        onBackClick={handleSetShowCharacterBio}
-                    />
-                )
-                : (
-                    <>
-                        <Slider />
-                        <section className={s.cardSection}>
-                            <Container>
-                                <div className={s.cardTitle}>
-                                    <Heading backLine>
-                                        Marvel Cards
-                                    </Heading>
-                                    <Heading level={2}>
-                                        Collect your best five cards
-                                    </Heading>
-                                </div>
-                                <div className={s.cardWrapper}>
-                                    {
-                                        characters.map(item => (
-                                            <div key={item.id}>
-                                                <CharacterCard
-                                                    src={item.thumbnail.path}
-                                                    onLikeClick={handleLikeClick}
-                                                    onReadBioClick={handleSetShowCharacterBio}
-                                                    {...item}
-                                                />
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            </Container>
-                        </section>
-                    </>
-                )
-            }
-            <Footer />
-        </>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/bio" element={<Biography id={1011334}/>}/>
+        </Routes>
     );
-}
+};
 
 export default App;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from '../../components/Header';
+import Slider from '../../components/Slider';
+import Container from '../../components/Container';
+import Heading from '../../components/Heading';
+import CharacterCard from '../../components/CharacterCard';
+import Footer from '../../components/Footer';
 import s from './Main.module.scss';
 
+import {CHARACTERS} from '../../mocks/characters';
+
 export const Main = () => {
+    const [showedIdCharacter, setShowedIdCharacter] = useState(null);
+    const [characters, setCharacters] = useState(CHARACTERS);
+    const handleLikeClick = (id) => {
+        setCharacters(prevState => prevState.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        isLike: !item.isLike
+                    }
+                }
+                return item;
+            })
+        );
+    };
+
+    const handleSetShowCharacterBio = (id) => setShowedIdCharacter(id);
+
     return (
-        <div className={s.root}>
-            MainPage
-        </div>
+        <>
+            <Header/>
+            <Slider/>
+            <section className={s.cardSection}>
+                <Container>
+                    <div className={s.cardTitle}>
+                        <Heading backLine>
+                            Marvel Cards
+                        </Heading>
+                        <Heading level={2}>
+                            Collect your best five cards
+                        </Heading>
+                    </div>
+                    <div className={s.cardWrapper}>
+                        {
+                            characters.map(item => (
+                                <div key={item.id}>
+                                    <CharacterCard
+                                        src={item.thumbnail.path}
+                                        onLikeClick={handleLikeClick}
+                                        onReadBioClick={handleSetShowCharacterBio}
+                                        {...item}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </Container>
+            </section>
+            <Footer/>
+        </>
     );
 };
