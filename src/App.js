a45bab9879d53e8e6ec7cf5371a2a5dd444f29f9
Nikolay Komarov <kny.komarov@gmail.com>
2022-05-10 18:01:33 +0300

    добавит scrollToTop

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import Characters from './pages/Characters';
 import Contacts from './pages/Contacts';
 import AboutGame from './pages/AboutGame';
 import NotFound from './pages/NotFound';
+import {ScrollToTop} from "./components/ScrollToTop/ScrollToTop";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Layout/>}>
-                <Route index element={<Main />} />
-                <Route exec path="characters" element={<Characters />} />
-                <Route exec path="contacts" element={<Contacts />} />
-                <Route exec path="about" element={<AboutGame />} />
-                <Route exec path="bio/:id" element={<Biography />}/>
-                <Route path="*" element={<NotFound />}/>
-            </Route>
-        </Routes>
+        <>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Main />} />
+                    <Route exec path="characters" element={<Characters />} />
+                    <Route exec path="contacts" element={<Contacts />} />
+                    <Route exec path="about" element={<AboutGame />} />
+                    <Route exec path="bio/:id" element={<Biography />}/>
+                    <Route path="*" element={<NotFound />}/>
+                </Route>
+            </Routes>
+        </>
     );
 };
 
